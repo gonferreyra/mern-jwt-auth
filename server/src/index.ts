045e78894d7a620aa27278a94850bcac7cd67ec0
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-09-30 15:21:08 -0300

    server connected and basic middlewares added

---
+++ server/src/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import connectToDatabase from './config/db';
+import { NODE_ENV, PORT, APP_ORIGIN } from './constants/env';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: APP_ORIGIN, // only this IP con access our api
+    credentials: true, // allow session cookie from browser to pass through
+  })
+);
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 'healthy',
+  });
+});
+
+// app.use(errorHandler);
+
+app.listen(8080, async () => {
+  console.log(`Server is running on port ${PORT} in ${NODE_ENV} enviroment.`);
+  await connectToDatabase();
+});
