299ee6e0bfc04d03da10b4d14023e4c73e79bdbb
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-10-01 16:44:58 -0300

    zod schemas moved to a new file

---
+++ server/src/controllers/auth.schemas.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+
+const emailSchema = z.string().email().min(1).max(255);
+const passwordSchema = z.string().min(6).max(255);
+const userAgentSchema = z.string().optional(); // to know where the request is coming
+
+export const loginSchema = z.object({
+  email: emailSchema,
+  password: passwordSchema,
+  userAgent: userAgentSchema,
+});
+
+export const registerSchema = loginSchema
+  .extend({
+    confirmPassword: passwordSchema,
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords do not match',
+    path: ['confirmPassword'],
+  });
