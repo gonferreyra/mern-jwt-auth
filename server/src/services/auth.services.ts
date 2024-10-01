38fc0af34c14cad43e039441cf6a0cef000a0341
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-10-01 11:45:42 -0300

    auth.service with createAcount logic

---
+++ server/src/services/auth.services.ts
@@ -0,0 +1,69 @@
+import VerificationCodeTypes from '../constants/verificationCodeTypes';
+import SessionModel from '../models/session.model';
+import UserModel from '../models/user.model';
+import VerificationCodeModel from '../models/verificationCode.model';
+import { oneYearFromNow } from '../utils/date';
+import { JWT_REFRESH_SECRET, JWT_SECRET } from '../constants/env';
+import jwt from 'jsonwebtoken';
+
+export type CreateAccoutParams = {
+  email: string;
+  password: string;
+  userAgent?: string;
+};
+
+export const createAccount = async (data: CreateAccoutParams) => {
+  // verify existing user
+  const existingUser = await UserModel.exists({
+    email: data.email,
+  });
+  if (existingUser) {
+    throw new Error('User already exists.');
+  }
+
+  // create user
+  const user = await UserModel.create({
+    email: data.email,
+    password: data.password,
+  });
+
+  // create verification code
+  const verificationCode = await VerificationCodeModel.create({
+    userId: user._id,
+    type: VerificationCodeTypes.EmailVerification,
+    expiresAt: oneYearFromNow(),
+  });
+
+  // send verification email
+  // create session
+  const session = await SessionModel.create({
+    userId: user._id,
+    userAgent: data.userAgent,
+  });
+
+  // sign access token & refresh token
+  const refreshToken = jwt.sign(
+    { sessionId: session._id },
+    JWT_REFRESH_SECRET,
+    {
+      audience: ['user'],
+      expiresIn: '30d',
+    }
+  );
+
+  const accessToken = jwt.sign(
+    { userId: user._id, sessionId: session._id },
+    JWT_SECRET,
+    {
+      audience: ['user'],
+      expiresIn: '15m',
+    }
+  );
+
+  // return user & tokens
+  return {
+    user,
+    accessToken,
+    refreshToken,
+  };
+};
