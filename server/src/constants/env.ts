b20b9208e0c44c304a51e88cfc4be75524cecc04
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-10-03 12:14:47 -0300

    resend api key added to env

--- server/src/constants/env.ts
+++ server/src/constants/env.ts
@@ -15,4 +15,4 @@ export const API_ORIGIN = getEnv('API_ORIGIN');
 export const JWT_SECRET = getEnv('JWT_SECRET');
 export const JWT_REFRESH_SECRET = getEnv('JWT_REFRESH_SECRET');
 // export const EMAIL_SENDER = getEnv('EMAIL_SENDER');
-// export const RESEND_API_KEY = getEnv('RESEND)API_KEY');
+export const RESEND_API_KEY = getEnv('RESEND_API_KEY');
