e25b20493586494e9991840b75b6b2191b55617d
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-10-03 16:59:29 -0300

    env variable typo fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
 import connectToDatabase from './config/db';
-import { NODE_ENV, PORT, API_ORIGIN } from './constants/env';
+import { NODE_ENV, PORT, APP_ORIGIN } from './constants/env';
 import cookieParser from 'cookie-parser';
 import { errorHandler } from './middleware/errorHandler';
 import authRoutes from './routes/auth.route';
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: API_ORIGIN, // only this IP con access our api
+    origin: APP_ORIGIN, // only this IP con access our api
     credentials: true, // allow session cookie from browser to pass through
   })
 );
