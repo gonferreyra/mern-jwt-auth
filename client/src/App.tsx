265c67f8cc435c05a1b43b02703bafd8894561de
Gonzalo Ferreyra <gonferreyra@gmail.com>
2024-10-07 09:46:08 -0300

    register, verifyEmail and forgotPassword components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,8 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Login from './pages/Login';
+import Register from './pages/Register';
+import VerifyEmail from './pages/VerifyEmail';
+import ForgotPassword from './pages/ForgotPassword';
 
 const router = createBrowserRouter([
   {
@@ -9,6 +12,18 @@ const router = createBrowserRouter([
   {
     path: '/login',
     element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/email/verify/:code',
+    element: <VerifyEmail />,
+  },
+  {
+    path: '/password/forgot',
+    element: <ForgotPassword />,
   },
 ]);
 
--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -5,4 +5,17 @@ type LoginParams = {
   password: string;
 };
 
+type RegisterParams = LoginParams & {
+  confirmPassword: string;
+};
+
 export const login = async (data: LoginParams) => API.post('/auth/login', data);
+
+export const register = async (data: RegisterParams) =>
+  API.post('/auth/register', data);
+
+export const verifyEmail = async (code: string) =>
+  API.get(`/auth/email/verify/${code}`);
+
+export const sendPasswordResetEmail = async (email: string) =>
+  API.post('/auth/password/forgot', { email });
